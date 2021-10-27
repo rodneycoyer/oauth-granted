2ed3ebae37847982141b546df82d4a73de577c6f
roofWalker <roofwalker13@outlook.com>
2021-10-27 10:33:35 -0700

    user.routes, server updates

---
+++ routes/user.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const User = require("../models/user.model");
+
+const userRouter = express.Router();
+
+// list all users
+userRouter.get("/", (req, res) => {
+
+});
+// create new user
+userRouter.post("/", (req, res) => {
+
+});
+// update user info
+userRouter.put("/", (req, res) => {
+
+});
+// delete user
+userRouter.delete("/", (req, res) => {
+
+});
